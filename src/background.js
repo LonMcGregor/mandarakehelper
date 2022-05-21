d0a90d66f9cad2fefe21ecb7276497624067087c
lonm <LonMcGregor@users.noreply.github.com>
2022-05-21 20:28:30 +0100

    make it work from main item pages instead

--- src/background.js
+++ src/background.js
@@ -1,42 +1,14 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.additem) {
-
-        chrome.storage.local.get({"thelist": []})
-
-        fetch(request.additem)
-        .then(response => response.text())
-        .then(bodytext => {
-            const parser = new DOMParser();
-            return parser.parseFromString(text, "text/html");
-        })
-        .then(body => {
-            // get main item title, store and price
-            let prices = [];
-            prices.push(
-                [
-                    body.querySelector(".content_head .shop").innerText,
-                    body.querySelector(".basicinfo .__price + p").innerText
-                ]
-            );
-            // get other alt stores and prices
-            prices = prices.concat(Array.from(body.querySelectorAll(".other_item .block")).map(x => [
-                x.querySelector(".shop").innerText,
-                x.querySelector(".price").innerText
-            ]));
-            // add all to storage
-            return {
-                "url": body.querySelector("link[hreflang=en]").href,
-                "title": body.querySelector("h1").innerText,
-                "prices": prices
-            };
-        })
-        .then(itemdata => {
-            return [Promise.resolve(chrome.storage.local.get({"thelist": "[]"})), itemdata];
-        })
-        .then(function ([storage, itemdata]){
+        chrome.storage.local.get({"thelist": "[]"})
+        .then(storage => {
+            // note: array concat returns a new array
+            // array.push modifies the original and returns the size
+            const thearray = JSON.parse(storage.thelist);
+            thearray.push(request.additem);
             return chrome.storage.local.set({
-                "thelist": JSON.stringify(JSON.parse(storage.thelist).push(itemdata))
+                "thelist": JSON.stringify(thearray)
             })
-        })
+        });
     }
 });
--- src/content.js
+++ src/content.js
@@ -1,11 +1,33 @@
-document.querySelectorAll(".content .block").forEach(saleitem => {
-    const btn = document.createElement("button");
-    btn.innerText = "compare";
-    btn.className = "extbtn";
-    btn.addEventListener("click", e => {
-        chrome.runtime.sendMessage({"additem": e.target.parentElement.querySelector("a").href}, response => {
-            e.target.innerText = "Added!";
-        })
+const btn = document.createElement("button");
+btn.innerText = "Add this item to extension list";
+btn.className = "extbtn";
+btn.addEventListener("click", add_this_item);
+document.querySelector(".operate").appendChild(btn);
+
+
+function add_this_item(e){
+    let prices = [];
+    prices.push(
+        [
+            document.querySelector(".content_head .shop").innerText,
+            document.querySelector(".basicinfo .__price + p").innerText
+        ]
+    );
+    // get other alt stores and prices
+    prices = prices.concat(Array.from(document.querySelectorAll(".other_item .block")).map(x => [
+        x.querySelector(".shop").innerText,
+        x.querySelector(".price").innerText
+    ]));
+    // add all to storage
+    data_item = {
+        "url": window.location.href,
+        "title": document.querySelector("h1").innerText,
+        "prices": prices
+    };
+    chrome.runtime.sendMessage({
+        "additem": JSON.stringify(data_item)
+    })
+    .then(response => {
+        e.target.innerText = "Added!";
     });
-    saleitem.appendChild(btn);
-})
+}
